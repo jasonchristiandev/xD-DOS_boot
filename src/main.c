2712cea5a7b543780487a3be5904e18625b6efe4
jasonchristiandev <jasonchristiandev@gmail.com>
2026-07-20 09:30:59 +0700

    bootloader now loads!

--- src/main.c
+++ src/main.c
@@ -43,6 +43,12 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
 																							 L"> Maintained by Jason Christian.\r\n\n"
 																							 L"> Press any key to continue...\r\n\n");
 
+	UINTN index;
+	EFI_EVENT events[1];
+	events[0] = SystemTable->ConIn->WaitForKey;
+	SystemTable->BootServices->WaitForEvent(1, events, &index);
+	SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
+
 	EFI_FILE_HANDLE volume = GetVolume(ImageHandle, SystemTable);
 	EFI_FILE_HANDLE file;
 	status = volume->Open(volume, &file, L"\\boot\\kernel.elf", EFI_FILE_MODE_READ, EFI_FILE_READ_ONLY | EFI_FILE_HIDDEN | EFI_FILE_SYSTEM);
@@ -55,10 +61,11 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
 	EFI_FILE_INFO *info = NULL;
 	EFI_GUID guid = EFI_FILE_INFO_ID;
 	status = file->GetInfo(file, &guid, &size, NULL);
+
 	if (status == EFI_BUFFER_TOO_SMALL) {
 		status = SystemTable->BootServices->AllocatePool(EfiLoaderData, size, (void **) &info);
 		if (EFI_ERROR(status)) {
-			Print(L"Failed to allocate pool for kernel\r\n");
+			Print(L"Failed to allocate pool for kernel info\r\n");
 			for (;;) {}
 		}
 
@@ -67,65 +74,84 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
 			Print(L"Failed to get kernel data\r\n");
 			for (;;) {}
 		}
+	}
 
-		VOID *base = NULL;
-		status = SystemTable->BootServices->AllocatePool(EfiLoaderData, info->FileSize, &base);
-		if (EFI_ERROR(status)) {
-			Print(L"Failed to allocate pool for kernel\r\n");
-			for (;;) {}
+	VOID *base = NULL;
+	status = SystemTable->BootServices->AllocatePool(EfiLoaderData, info->FileSize, &base);
+	if (EFI_ERROR(status)) {
+		Print(L"Failed to allocate pool for kernel\r\n");
+		for (;;) {}
+	}
+
+	UINTN read_size = info->FileSize;
+	status = file->Read(file, &read_size, base);
+	if (EFI_ERROR(status)) {
+		Print(L"Failed to get kernel file size\r\n");
+		for (;;) {}
+	}
+
+	Elf64_Ehdr *ehdr = (Elf64_Ehdr *) base;
+	UINT64 entry = ehdr->e_entry;
+
+	status = SystemTable->BootServices->FreePool(info);
+	if (EFI_ERROR(status)) {
+		Print(L"Failed to free file info pool\r\n");
+		for (;;) {}
+	}
+
+	Elf64_Phdr *phdr = (Elf64_Phdr *) ((UINT8 *) base + ehdr->e_phoff);
+	for (UINT16 i = 0; i < ehdr->e_phnum; i++) {
+		if (phdr[i].p_type == PT_LOAD) {
+			UINT64 virt = phdr[i].p_vaddr;
+			UINT64 phys = virt >= 0xFFFFFFFF80000000 ? virt - 0xFFFFFFFF80000000 : virt;
+
+			VOID *dest = (VOID *) phys;
+			VOID *src = (VOID *) ((UINT8 *) base + phdr[i].p_offset);
+
+			SystemTable->BootServices->CopyMem(dest, src, phdr[i].p_filesz);
+
+			if (phdr[i].p_memsz > phdr[i].p_filesz) {
+				SystemTable->BootServices->SetMem((UINT8 *) dest + phdr[i].p_filesz, phdr[i].p_memsz - phdr[i].p_filesz, 0);
+			}
 		}
+	}
 
-		UINTN read_size = info->FileSize;
-		status = file->Read(file, &read_size, base);
-		if (EFI_ERROR(status)) {
-			Print(L"Failed to get kernel file size\r\n");
-			for (;;) {}
-		}
+	EFI_MEMORY_DESCRIPTOR *memmap = NULL;
+	UINTN memmap_key = 0;
+	UINTN desc_size = 0;
+	UINT32 desc_ver = 0;
+	UINTN memmap_size = 0;
 
-		Elf64_Ehdr *ehdr = (Elf64_Ehdr *) base;
-		UINT64 entry = ehdr->e_entry;
-
-		status = SystemTable->BootServices->FreePool(info);
-		if (EFI_ERROR(status)) {
-			Print(L"Failed to get free file info pool\r\n");
-			for (;;) {}
-		}
-
-		EFI_MEMORY_DESCRIPTOR *memmap = NULL;
-		UINTN memmap_key;
-		UINTN desc_size;
-		UINT32 desc_ver;
-		UINTN memmap_size = 0;
-
-		status = SystemTable->BootServices->GetMemoryMap(&memmap_size, NULL, &memmap_key, &desc_size, &desc_ver);
-		if (EFI_ERROR(status)) {
-			Print(L"Failed to get memory map\r\n");
-			for (;;) {}
-		}
-
-		memmap_size += (2 * desc_size);
-		status = SystemTable->BootServices->AllocatePool(EfiLoaderData, memmap_size, (void **) &memmap);
-		if (EFI_ERROR(status)) {
-			Print(L"Failed to get allocate pool for memory map\r\n");
-			for (;;) {}
-		}
-
+	while (TRUE) {
 		status = SystemTable->BootServices->GetMemoryMap(&memmap_size, memmap, &memmap_key, &desc_size, &desc_ver);
-		if (EFI_ERROR(status)) {
+
+		if (status == EFI_BUFFER_TOO_SMALL) {
+			if (memmap != NULL) {
+				SystemTable->BootServices->FreePool(memmap);
+				memmap = NULL;
+			}
+
+			memmap_size += (2 * desc_size);
+			status = SystemTable->BootServices->AllocatePool(EfiLoaderData, memmap_size, (void **) &memmap);
+			if (EFI_ERROR(status)) {
+				Print(L"Failed to allocate pool for memory map\r\n");
+				for (;;) {}
+			}
+			continue;
+		} else if (EFI_ERROR(status)) {
 			Print(L"Failed to get memory map\r\n");
 			for (;;) {}
 		}
 
 		status = SystemTable->BootServices->ExitBootServices(ImageHandle, memmap_key);
-		if (EFI_ERROR(status)) {
-			Print(L"Failed to exit boot services\r\n");
-			for (;;) {}
+		if (!EFI_ERROR(status)) {
+			break;
 		}
-
-		typedef void (*KernelEntry)(void);
-		KernelEntry jump = (KernelEntry) entry;
-		// jump();
 	}
+
+	typedef void (*KernelEntry)(void);
+	KernelEntry jump = (KernelEntry) (entry - 0xFFFFFFFF80000000);
+	jump();
 
 	return EFI_SUCCESS;
 }
